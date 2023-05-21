7f9771511d32f83e753ee33c9c07c71be5af8f95
Juosorioca420 <juosorioca@unal.edu.co>
2023-05-20 20:38:47 -0500

    Definicion de los Controladores

--- src/main/java/com/shortener/URLShortener/UrlController.java
+++ src/main/java/com/shortener/URLShortener/UrlController.java
@@ -1,15 +1,15 @@
 package com.shortener.URLShortener;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod; //Request Mapping to redirect to index.HTML file (HTML Template - FrontEnd)
+import org.springframework.stereotype.Controller; // @Controller
+import org.springframework.web.bind.annotation.RequestMapping; // @RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod; //RequestMethod to redirect to index.HTML file (HTML Template - FrontEnd)
 
 @Controller
 public class UrlController {
 	
-	// Implementar REQUESTMAPPING  -   Default PATH:  value = '/'
+	/* Implementar REQUESTMAPPING  -   Default PATH:  value = '/' */
 	@RequestMapping(value="/", method=RequestMethod.GET)  //SpringBoot Method
 	public String loadIndex() {
-		return "index";
+		return "index"; // Retorna el nombre de una vista (HTML template) a mostrar
 	}
 }
---
+++ src/main/java/com/shortener/URLShortener/UrlRestController.java
@@ -0,0 +1,50 @@
+package com.shortener.URLShortener;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity; // Respuesta HTTP que se enviará al cliente desde un controlador.
+import java.net.MalformedURLException; // Error en caso la url este incorrecta y no arroje algún vinculo 
+
+import org.springframework.web.bind.annotation.RequestBody; // Importa @RequestBody
+import org.springframework.web.bind.annotation.RequestMapping; // Importa @RequestMapping
+import org.springframework.web.bind.annotation.RestController; // Importa @RestController
+
+import org.springframework.web.bind.annotation.RequestMethod; // RequestMethod.POST (enviar información al servidor)
+
+@RestController
+public class UrlRestController {
+    
+    private HashMap<String, ShortUrl> shorturlList = new HashMap<>();
+    private void crearUrl(String r, ShortUrl shortenUrl){
+        shortenUrl.setShort("http://localhost:8080/s/" + r);
+        shorturlList.put(r, shortenUrl);
+   }
+
+    /*  
+    Controlador que maneja las solicitudes POST a la URL /shortenurl.
+    Recibe un objeto ShortUrl en el cuerpo de la solicitud,
+    genera un carácter aleatorio para la URL acortada
+    y establece la URL acortada asociada (r) para la shorturl entrante.
+    Luego, devuelve un objeto ResponseEntity que contiene 'r' como respuesta HTTP.
+    */
+	@RequestMapping(value="/shortenurl", method = RequestMethod.POST) // Mapear una URL específica (/shortenurl) a un método getShortUrl(). 
+    // Recibe un objeto ShortUrl como parámetro, que se obtiene del cuerpo de la solicitud utilizando la anotación @RequestBody. 
+	public ResponseEntity<Object>  getShortUrl(@RequestBody ShortUrl shorturl) throws MalformedURLException {
+		String r = randChar();
+		crearUrl(r, shorturl); // Establece la URL acortada asociada (r) para el shorturl recibido.
+
+		return new ResponseEntity<Object>(shorturl, HttpStatus.OK); // HttpStatus.OK para indicar que la solicitud se ha procesado correctamente.
+	}
+
+    // Generador de cadenas alfanumericas aleatorias.
+	private String randChar() {
+		String str = ""; Random r = new Random();
+		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for (int i = 0; i < 5; i++)
+			str += chars.charAt( r.nextInt(str.length()) );
+		return str;
+	}
+
+}
